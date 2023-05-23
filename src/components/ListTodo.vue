0442e7ac4b9905917aeb59135939b518b8a8e6fd
Faisal Chakiki <faisalchakiki012018@gmail.com>
2023-05-23 08:36:47 +0700

    feat: initial commit

---
+++ src/components/ListTodo.vue
@@ -0,0 +1,91 @@
+<template>
+  <div v-if="dataTasks.length >= 1" v-for="(item, index) in dataTasks" :key="index">
+    <div v-if="item?.levelTask === 'Easy'" class="wrapper-card">
+      <div class="card easy">
+        <p>{{item?.taskTitle}}</p>
+      </div>
+      <button @click="deleteTask(item?.id)" class="delete">Delete</button>
+    </div>
+    <div v-else-if="item?.levelTask === 'Medium'" class="wrapper-card">
+      <div class="card medium">
+        <p>{{item?.taskTitle}}</p>
+      </div>
+      <button @click="deleteTask(item?.id)" class="delete">Delete</button>
+    </div>
+    <div v-else-if="item?.levelTask === 'Hard'" class="wrapper-card">
+      <div class="card hard">
+        <p>{{item?.taskTitle}}</p>
+      </div>
+      <button @click="deleteTask(item?.id)" class="delete">Delete</button>
+    </div>
+  </div>
+  <div v-else class="center">
+    <p>-- Task Not Found --</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props:{
+    dataTasks:{
+      type: Array,
+      required: false,
+      default: []
+    },   
+  },
+  methods:{
+    deleteTask(id : number){
+      if(confirm('Are you sure want to remove task?')){
+        return this.$emit("deleteId", id);
+      }
+      return alert('Good Luck')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.wrapper-card{
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+.card {
+  width: 75%;
+  color: #fefefe;
+  padding: 0 2rem;
+  text-align: left;
+  border-radius: 8px;
+  letter-spacing: 1.5px;
+  text-transform: capitalize;
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.delete {
+  background-color: #939393;
+  font-size: 20px;
+  width: 15%;
+}
+
+.easy {
+  background-color:#5dda1e ;
+}
+
+.medium {
+  background-color:#cfe909 ;
+}
+
+.hard{
+  background-color:#ff5653 ;
+}
+
+.center{
+  width: 100%;
+  font-weight: 600;
+  text-align: center;
+  font-size: 18px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+ <form @submit.prevent="onSubmit">
+  <div class="navbar">
+    <input v-model="taskTitle" type="text" placeholder="Add a task." />
+    <select v-model="levelTask">
+      <option disabled value="">Please select one</option>
+      <option>Easy</option>
+      <option>Medium</option>
+      <option>Hard</option>
+    </select>
+    <button>Add Task</button>
+  </div>
+ </form>
+</template>
+
+<script lang="ts">
+
+interface DataTask {
+    id : number
+    taskTitle: string
+    levelTask : 'Easy' | 'Medium' | 'Hard'
+}
+
+export default {
+  data() : DataTask{
+    return {
+      id : Date.now(),
+      taskTitle: '',
+      levelTask: 'Easy',
+    }
+  },
+  methods:{
+    clearData(){
+        this.id = Date.now();
+        this.taskTitle = '';
+        this.levelTask = 'Easy';
+    },
+    onSubmit(){
+      if(this.taskTitle === ''){
+        alert('Please fill all the input')
+        return false;
+      }
+      // $emit = mengirimkan acara dengan nama dan data ke komponen induk.
+      this.$emit("dataToParent", this.$data);
+      this.clearData()
+    }
+  },
+}
+</script>
+
+<style>
+  .navbar {
+  background-color: #e7e7e7;
+  width: 100%;
+  border-radius: 8px;
+  padding: 20px 5%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  margin-bottom: 35px;
+}
+
+</style>
